0719abf2edf71b97760a12fb65f7080c09788605
Cyanox <Infernogaming621@gmail.com>
2020-07-24 20:05:40 -0400

    Fixed configs

--- SerpentsHand/Config.cs
+++ SerpentsHand/Config.cs
@@ -7,18 +7,18 @@ namespace SerpentsHand
     {
 		public bool IsEnabled { get; set; } = true;
 
-		public List<int> SpawnItems = new List<int>() { 21, 26, 12, 14, 10 };
+		public List<int> SpawnItems { get; set; } = new List<int>() { 21, 26, 12, 14, 10 };
 
-		public int SpawnChance = 50;
-		public int Health = 120;
-		public int MaxSquad = 8;
-		public int RespawnDelay = 1;
+		public int SpawnChance { get; set; } = 50;
+		public int Health { get; set; } = 120;
+		public int MaxSquad { get; set; } = 8;
+		public int RespawnDelay { get; set; } = 1;
 
-		public string EntryAnnouncement = "SERPENTS HAND HASENTERED";
-		public string CiEntryAnnouncement = "";
+		public string EntryAnnouncement { get; set; } = "SERPENTS HAND HASENTERED";
+		public string CiEntryAnnouncement { get; set; } = "";
 
-		public bool FriendlyFire = false;
-		public bool TeleportTo106 = true;
-		public bool ScpsWinWithChaos = true;
+		public bool FriendlyFire { get; set; } = false;
+		public bool TeleportTo106 { get; set; } = true;
+		public bool ScpsWinWithChaos { get; set; } = true;
     }
 }
