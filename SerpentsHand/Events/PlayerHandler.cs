6b0491a4f1c9b856b66d746111251a143778fe89
Thunder <thundermaker300@gmail.com>
2023-02-05 18:58:45 -0500

    Fix some compiler

--- SerpentsHand/Events/PlayerHandler.cs
+++ SerpentsHand/Events/PlayerHandler.cs
@@ -1,6 +1,8 @@
 using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
 using PlayerStatsSystem;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,16 +36,16 @@ namespace SerpentsHand.Events
             if (ev.Attacker == null)
                 return;
 
-            if (((API.IsSerpent(ev.Target) && (ev.Attacker.Role.Team == Team.SCP || ev.Handler.Type == DamageType.PocketDimension)) ||
-                (API.IsSerpent(ev.Attacker) && (ev.Target.Role.Team == Team.SCP || (scp035s != null && scp035s.Contains(ev.Target)))) ||
-                (API.IsSerpent(ev.Target) && API.IsSerpent(ev.Attacker) && ev.Target != ev.Attacker)) && !config.SerpentsHandModifiers.FriendlyFire)
+            if (((API.IsSerpent(ev.Player) && (ev.Attacker.Role.Team == Team.SCPs || ev.DamageHandler.Type == DamageType.PocketDimension)) ||
+                (API.IsSerpent(ev.Attacker) && (ev.Player.Role.Team == Team.SCPs || (scp035s != null && scp035s.Contains(ev.Player)))) ||
+                (API.IsSerpent(ev.Player) && API.IsSerpent(ev.Attacker) && ev.Player != ev.Attacker)) && !config.SerpentsHandModifiers.FriendlyFire)
                 ev.IsAllowed = false;
         }
 
         public void OnShooting(ShootingEventArgs ev)
         {
             Player target = Player.Get(ev.TargetNetId);
-            if (target != null && target.Role == RoleType.Scp096 && API.IsSerpent(ev.Shooter))
+            if (target != null && target.Role == RoleTypeId.Scp096 && API.IsSerpent(ev.Player))
                 ev.IsAllowed = false;
         }
 
@@ -53,11 +55,11 @@ namespace SerpentsHand.Events
                 ev.IsAllowed = false;
         }
 
-        public void OnEnteringFemurBreaker(EnteringFemurBreakerEventArgs ev)
+        /*public void OnEnteringFemurBreaker(EnteringFemurBreakerEventArgs ev)
         {
             if (API.IsSerpent(ev.Player) && !config.SerpentsHandModifiers.FriendlyFire)
                 ev.IsAllowed = false;
-        }
+        }*/
 
         public void OnDestroying(DestroyingEventArgs ev)
         {
@@ -67,32 +69,31 @@ namespace SerpentsHand.Events
 
         public void OnDied(DiedEventArgs ev)
         {
-            if (API.IsSerpent(ev.Target))
+            if (API.IsSerpent(ev.Player))
             {
-                Extensions.DestroySH(ev.Target);
+                Extensions.DestroySH(ev.Player);
                 return;
             }
 
-            if (ev.Target.Role == RoleType.Scp106 && !config.SerpentsHandModifiers.FriendlyFire)
+            if (ev.Player.Role == RoleTypeId.Scp106 && !config.SerpentsHandModifiers.FriendlyFire)
                 foreach (Player player in Player.List.Where(x => x.CurrentRoom.Type == RoomType.Pocket))
                     player.Hurt(new CustomReasonDamageHandler("WORLD", 50000f));
         }
 
         public void OnChangingRole(ChangingRoleEventArgs ev)
         {
-            if (API.IsSerpent(ev.Player) && ev.NewRole != RoleType.Tutorial)
+            if (API.IsSerpent(ev.Player) && ev.NewRole != RoleTypeId.Tutorial)
                 Extensions.DestroySH(ev.Player);
         }
 
         public void OnSpawningRagdoll(SpawningRagdollEventArgs ev)
         {
-            if (!API.IsSerpent(ev.Owner))
+            if (!API.IsSerpent(ev.Player))
                 return;
 
             ev.IsAllowed = false;
 
-            RagdollInfo info = new RagdollInfo(Server.Host.ReferenceHub, ev.DamageHandlerBase, ev.Role, ev.Position, ev.Rotation, ev.Nickname, ev.CreationTime);
-            new Exiled.API.Features.Ragdoll(info, true);
+            Ragdoll.CreateAndSpawn(ev.Role, ev.Nickname, ev.DamageHandlerBase, ev.Position, ev.Rotation);
         }
     }
 }
